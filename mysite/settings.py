80111dd6b89405213ded02fcfc06fd61bdaa80c1
Jaime <kovenam@gmail.com>
2024-03-02 00:54:14 +0100

    restore URL

--- mysite/settings.py
+++ mysite/settings.py
@@ -94,7 +94,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         # Replace this value with your local database's connection string.
-        default='postgresql://postgres:postgres@localhost/postgres',
+        default='postgresql://postgres:postgres@localhost:5432/mysite',
         conn_max_age=600
     )
 }
